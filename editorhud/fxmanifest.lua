fcdaa550fe197b08b3a20d0ec0b28187a1437b87
Matthias Klingner <klingni@hotmail.com>
2021-12-19 18:50:50 +0100

    Change description

--- editorhud/fxmanifest.lua
+++ editorhud/fxmanifest.lua
@@ -2,7 +2,7 @@ resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 fx_version 'adamant'
 game 'gta5'
 
-description 'Scoreboard'
+description 'CNC Editor User Interface'
 
 -- temporary!
 ui_page 'html/hud.html'
